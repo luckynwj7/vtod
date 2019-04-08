6d209b58390fb105a7dead4c1762c2d27c2a5f76
luckynwj7 <luckynwj7@naver.com>
2019-04-08 18:12:12 +0900

    Initial commit

---
+++ app/src/main/jni/opencv_java4.c
@@ -0,0 +1,98 @@
+#include <jni.h>
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_vtod_MainActivity_detect(JNIEnv *env, jobject instance,
+                                          jlong cascadeClassifier_face, jlong cascadeClassifier_eye,
+                                          jlong matAddrInput, jlong matAddrResult) {
+
+    // TODO
+
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_vtod_MainActivity_detect(JNIEnv *env, jobject instance,
+                                          jlong cascadeClassifier_face, jlong cascadeClassifier_eye,
+                                          jlong matAddrInput, jlong matAddrResult) {
+
+    // TODO
+
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_vtod_MainActivity_detect(JNIEnv *env, jobject instance,
+                                          jlong cascadeClassifier_face, jlong cascadeClassifier_eye,
+                                          jlong matAddrInput, jlong matAddrResult) {
+
+    // TODO
+
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
+                                               jstring cascadeFileName_) {
+    const char *cascadeFileName = (*env)->GetStringUTFChars(env, cascadeFileName_, 0);
+
+    // TODO
+
+    (*env)->ReleaseStringUTFChars(env, cascadeFileName_, cascadeFileName);
+}
